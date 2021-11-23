e80e8df4d4379ef6e1657c2ee1b6e14680a108ff
ch-pat <patryk.choma@gmail.com>
2021-11-23 17:27:30 +0100

    10

---
+++ 10.py
@@ -0,0 +1,32 @@
+"""Summation of primes
+The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
+
+Find the sum of all the primes below two million.
+"""
+from math import sqrt
+
+def is_prime(n):
+    if n in (0, 1):
+        return False
+    factors = {1, n}
+    for i in range(1, int(sqrt(n)) + 1):
+        if n % i == 0 and i not in factors:
+            return False
+    return True
+
+def prime_sum(n):
+    candidates = [x for x in range(3, n + 1, 2)]
+    total = 0
+    primes = [2]
+    counter = 15
+    while candidates and counter:
+        counter -= 1
+        current = candidates[0]
+        primes.append(current)
+        candidates = [x for x in candidates if x % current]
+    for x in candidates:
+        if is_prime(x):
+            primes.append(x)
+    return sum(primes)
+
+print(prime_sum(2000000))
--- 3.py
+++ 3.py
@@ -5,10 +5,11 @@ The prime factors of 13195 are 5, 7, 13 and 29.
 What is the largest prime factor of the number 600851475143 ?
 """
 from math import sqrt
-
 def is_prime(n):
+    if n in (0, 1):
+        return False
     factors = {1, n}
-    for i in range(1, int(sqrt(n))):
+    for i in range(1, int(sqrt(n)) + 1):
         if n % i == 0 and i not in factors:
             return False
     return True
