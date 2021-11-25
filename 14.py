c8547d04bfd361dff89f6cb7ccd43d63d19f52ed
ch-pat <patryk.choma@gmail.com>
2021-11-25 16:19:54 +0100

    14

--- 14.py
+++ 14.py
@@ -13,34 +13,74 @@ Which starting number, under one million, produces the longest chain?
 
 NOTE: Once the chain starts the terms are allowed to go above one million.
 """
-import time
-memo = {1 : [1]}
+def calculate_whole_sequences():
+    import time
+    memo = {1 : [1]}
 
-def collatz_sequence(n, memo):
-    if n in memo:
-        return memo[n]
-    if n % 2:
-        sequence = [n] + collatz_sequence(n * 3 + 1, memo)
-        memo[n] = sequence
-        return sequence 
-    else:
-        sequence = [n] + collatz_sequence(n // 2, memo)
-        memo[n] = sequence
-        return sequence
+    def collatz_sequence(n, memo):
+        if n in memo:
+            return memo[n]
+        if n % 2:
+            sequence = [n] + collatz_sequence(n * 3 + 1, memo)
+            memo[n] = sequence
+            return sequence 
+        else:
+            sequence = [n] + collatz_sequence(n // 2, memo)
+            memo[n] = sequence
+            return sequence
 
 
-start = time.perf_counter()
-limit = 1000000
+    start = time.perf_counter()
+    limit = 1000000
 
-for i in range(1, limit + 1):
-    collatz_sequence(i, memo)
+    for i in range(1, limit + 1):
+        collatz_sequence(i, memo)
 
-longest = []
-for n in range(1, 1000001):
-    current = memo[n]
-    if len(current) > len(longest):
-        longest = current
-print(longest)
+    longest = []
+    for n in range(1, 1000001):
+        current = memo[n]
+        if len(current) > len(longest):
+            longest = current
+    print(longest)
 
-end = time.perf_counter() - start
-print(end)
+    end = time.perf_counter() - start
+    print(end)
+
+
+def lengths_only():
+    import time
+    memo = {1 : 1}
+
+    def collatz_sequence(n, memo):
+        if n in memo:
+            return memo[n]
+        if n % 2:
+            sequence = 1 + collatz_sequence(n * 3 + 1, memo)
+            memo[n] = sequence
+            return sequence 
+        else:
+            sequence = 1 + collatz_sequence(n // 2, memo)
+            memo[n] = sequence
+            return sequence
+
+
+    start = time.perf_counter()
+    limit = 1000000
+
+    for i in range(1, limit + 1):
+        collatz_sequence(i, memo)
+
+    longest = 0
+    cur_idx = 0
+    for n in range(1, 1000001):
+        current = memo[n]
+        if current > longest:
+            longest = current
+            cur_idx = n
+    print(cur_idx, memo[cur_idx])
+
+    end = time.perf_counter() - start
+    print(end)
+
+if __name__ == "__main__":
+    lengths_only()
